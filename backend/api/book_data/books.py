9fa3d29a24b42d19ebc631b79fe77e05920c0121
James Jester <elmojester@gmail.com>
2022-11-01 02:04:31 -0500

    Book seed data

---
+++ backend/api/book_data/books.py
@@ -0,0 +1,79 @@
+books = [
+    {
+        'isbn': 9781108826822,
+        'title': 'Cambridge KJV Family Chronicle Bible', 
+        'author': 'Cambridge Bible', 
+        'description': 'The Cambridge Family Chronicle Bible has been designed and produced as a Bible to enjoy for generations. It combines the best typographic design with the highest standards of printing and bookbinding. The majestic text of the King James Bible is presented in a typesetting inspired by the legendary Baskerville Bible, and the words of Scripture are brought to life with 221 engravings by 19th century illustrator Gustave Doré – painstakingly reproduced for this edition from the original printings.', 
+        'price': 9.99, 
+        'category': 'Spiritual', 
+        'stock_quantity': 10,
+        'image_file': 'bible.jpg'
+        
+    },
+    {
+        'isbn': 978321321321,
+        'title': 'Norse Mythology', 
+        'author': 'Neil Gaiman', 
+        'description': 'Gaiman fashions these primeval stories into a novelistic arc that begins with the genesis of the legendary nine worlds and delves into the exploits of deities, dwarfs, and giants. Once, when Thor’s hammer is stolen, Thor must disguise himself as a woman―difficult with his beard and huge appetite―to steal it back. More poignant is the tale in which the blood of Kvasir―the most sagacious of gods―is turned into a mead that infuses drinkers with poetry. The work culminates in Ragnarok, the twilight of the gods and rebirth of a new time and people.', 
+        'price': 9.99, 
+        'category': 'Fiction', 
+        'stock_quantity': 10,
+        'image_file': 'norse.jpg'
+        
+    },
+    {
+        'isbn': 9781512308051,
+        'title': 'Frankenstein', 
+        'author': 'Mary Shelley', 
+        'description': 'Frankenstein; or, The Modern Prometheus, is a novel written by English author Mary Shelley about the young student of science Victor Frankenstein, who creates a grotesque but sentient creature in an unorthodox scientific experiment. Shelley started writing the story when she was eighteen, and the novel was published when she was twenty. The first edition was published anonymously in London in 1818.', 
+        'price': 9.99, 
+        'category': 'Fiction', 
+        'stock_quantity': 10,
+        'image_file': 'frank.jpg'
+        
+    },
+    {
+        'isbn': 9780062390622,
+        'title': 'The Alchemist', 
+        'author': 'Paulo Coelho', 
+        'description': 'The Alchemist by Paulo Coelho continues to change the lives of its readers forever. With more than two million copies sold around the world, The Alchemist has established itself as a modern classic, universally admired. Paulo Coelho’s masterpiece tells the magical story of Santiago, an Andalusian shepherd boy who yearns to travel in search of a worldly treasure as extravagant as any ever found.', 
+        'price': 9.99, 
+        'category': 'Fiction', 
+        'stock_quantity': 10,
+        'image_file': 'alchemist.jpg'
+        
+    },
+    {
+        'isbn': 9789401462617,
+        'title': 'FUNdamental Mathematics: A voyage into the quirky universe of maths & jokes.', 
+        'author': 'David Eelbode', 
+        'description': 'Mathematics does not have to be boring. On the contrary: it can be very funny. In this book David Eelbode takes you on a fascinating trip to the mathematical concept of space through personal anecdotes, sharp observations and absurd reasoning. To make the difficult parts more understandable he uses a proven concept: humor. The reader of this book will not only learn about mathematics but also about the enigmatic community of people who are working on and in this fascinating world. David Eelbode is able to capture our attention with original and intriguing metaphors.', 
+        'price': 9.99, 
+        'category': 'Math', 
+        'stock_quantity': 10,
+        'image_file': 'math.jpg'
+        
+    },
+    {
+        'isbn': 9781947501249,
+        'title': 'The American Story: The Beginnings',
+        'author': 'David Barton',
+        'description': 'AMERICA IS UNIQUE. No other nation in the world has experienced the widespread levels of stability, individual prosperity, and personal freedoms that have defined this nation. What made us different from other nations? Was it the people, the principles, or the blessing of God? It was all three, and they caused America to become the most exceptional nation in the history of the world. ',
+        'price': 26.99,
+        'category': 'History',
+        'stock_quantity': 10,
+        'image_file': 'am_hist.jpg'
+
+    },
+    {
+        'isbn': 9781465482474,
+        'title': 'History of Britain and Ireland: The Definitive Visual Guide',
+        'author': 'DK',
+        'description': 'Combining over 700 photographs, maps, and artworks with accessible text, the History of Britain and Ireland is an invaluable resource for families, students, and anyone seeking to learn more about the fascinating story of the England, Scotland, Wales, and Ireland. Spanning six distinct periods of British and Irish history, this ebook is the best way to find out how Britain transformed with the Norman rule, fought two world wars in the 20th century, and faced new economic challenges in the 21st century.',
+        'price': 18.67,
+        'category': 'History',
+        'stock_quantity': 10,
+        'image_file': 'brit_hist.jpg'
+
+    },
+]
